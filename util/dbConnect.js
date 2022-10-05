a31070d38d282f20da6650f645e18ced1e8afa2a
Shuvam <shuvamsantra10@gmail.com>
2022-10-05 13:24:03 +0530

    Completed

--- util/dbConnect.js
+++ util/dbConnect.js
@@ -4,10 +4,10 @@ dotenv.config();
 
 const url = process.env.MONGO_URI;
 
-mongoose.connect(url).then(() => {
+const connect = mongoose.connect(url).then(() => {
     console.log("Connection Succesful");
 }).catch(() => {
     console.log("No connection")
 });
-export default url;
+export default connect;
 
