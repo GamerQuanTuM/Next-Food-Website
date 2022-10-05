8a713c0bb90469a999829d57afb596b7d7225c9f
Shuvam <shuvamsantra10@gmail.com>
2022-10-05 13:07:11 +0530

    Completed

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,16 @@ import Layout from "../components/Layout";
 import "../styles/globals.css";
 import store from "../redux/store";
 import { Provider } from "react-redux";
-import ErrorBoundary from "../components/_error";
+
 import React from "react";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ErrorBoundary>
       <Provider store={store}>
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </Provider>
-    </ErrorBoundary>
   );
 }
 
