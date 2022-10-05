0d46da07bbf0660fc8d62835217ddaaeeaacaaeb
Shuvam <shuvamsantra10@gmail.com>
2022-10-05 12:10:40 +0530

    Completed

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,20 +1,21 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "../styles/ProductCard.module.scss";
+import React from "react";
 
 const ProductCard = (props) => {
   const { products } = props;
   return (
-    <>
+    <React.Fragment>
       <div className={styles.productCardContainer}>
-        <a href={`/products/${products._id}`}>
-          <Image src={products?.image} alt="img" width="500" height="500" />
-        </a>
+        <Link href={`/products/${products._id}`} passHref>
+          <a><Image src={products?.image} alt="img" width="500" height="500" /></a>
+        </Link>
         <h1 className={styles.title}>{products?.title}</h1>
         <span className={styles.price}>${products?.prices[0]}</span>
         <p className={styles.desc}>{products?.description}</p>
       </div>
-    </>
+    </React.Fragment>
   );
 };
 
