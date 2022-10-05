4c3eb25007a7acca36b13450195a52e551b44861
Shuvam <shuvamsantra10@gmail.com>
2022-10-05 11:45:17 +0530

    Completed

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -5,14 +5,16 @@ import styles from "../styles/ProductCard.module.scss";
 const ProductCard = (props) => {
   const { products } = props;
   return (
-    <div className={styles.productCardContainer}>
-      <Link href={`/products/${products._id}`} passHref>
-        <Image src={products?.image} alt="img" width="500" height="500" />
-      </Link>
-      <h1 className={styles.title}>{products?.title}</h1>
-      <span className={styles.price}>${products?.prices[0]}</span>
-      <p className={styles.desc}>{products?.description}</p>
-    </div>
+    <>
+      <div className={styles.productCardContainer}>
+        <a href={`/products/${products._id}`}>
+          <Image src={products?.image} alt="img" width="500" height="500" />
+        </a>
+        <h1 className={styles.title}>{products?.title}</h1>
+        <span className={styles.price}>${products?.prices[0]}</span>
+        <p className={styles.desc}>{products?.description}</p>
+      </div>
+    </>
   );
 };
 
